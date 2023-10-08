65d04042c6cb063185496ac9976d6f293d82cb69
radespratama <puturades89@gmail.com>
2023-10-08 22:18:07 +0800

    feat: add task 15

--- 15_intro_restful_api/praktikum/skenario_1/main.go
+++ 15_intro_restful_api/praktikum/skenario_1/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+type PostResponse struct {
+	UserId int    `json:"userId"`
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
+const API = "https://jsonplaceholder.typicode.com/posts"
+
+func getAllPost(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get(API)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var posts []PostResponse
+	err = json.Unmarshal(body, &posts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write(body)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	router := http.NewServeMux()
+
+	router.HandleFunc("/posts", getAllPost)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
